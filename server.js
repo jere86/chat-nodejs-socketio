41793881620d676c505967d2d61615cdf08a082c
Jerko Sarić <saricjerko86@gmail.com>
2023-03-10 20:45:08 +0100

    chat setup

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const io = require('socket.io')(3000, {
     cors: {
         origin: '*',
     }
-})
+});
 
 io.on('connection', socket => {
-    socket.emit('chat-message', 'Hello World')
-})
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', message);
+    })
+});
